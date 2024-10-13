b6c22fd54634a833e7761705feedabab133774a6
Buravit Yenjit <khiaokrub@gmail.com>
2024-10-13 22:32:27 +0700

    Made card1-3 link to their own page

--- moneymap/urls.py
+++ moneymap/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = "moneymap"
 urlpatterns = [
     path('', views.home, name='home'),
+    path('income-and-expenses/', views.income_and_expenses, name='income-expenses'),
+    path('goals/', views.goals, name='goals'),
+    path('history/', views.history, name='history'),
 ]
--- moneymap/views.py
+++ moneymap/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'moneymap/home.html')
 
+def income_and_expenses(request):
+    return render(request, 'moneymap/income-expenses.html')
+
+def goals(request):
+    return render(request, 'moneymap/goals.html')
+
+def history(request):
+    return render(request, 'moneymap/history.html')
