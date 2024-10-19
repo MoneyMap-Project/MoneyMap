52959ec893338de6d9f455896fbec589e0aebec9
Sittanat <apalakawongna@gmail.com>
2024-10-19 19:34:49 +0700

    create allauth

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('moneymap.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
 ]
