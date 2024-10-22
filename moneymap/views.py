2c5112c6134eda6cbaff8b7133d0dd01cda14bee
Buravit Yenjit <khiaokrub@gmail.com>
2024-10-22 22:54:20 +0700

    Modify income_and_expenses_view to sent today's balance

--- moneymap/views.py
+++ moneymap/views.py
@@ -12,14 +12,40 @@ def is_admin(user):
 def home(request):
     return render(request, 'moneymap/home.html')
 
+
 @login_required
 def income_and_expenses_view(request):
     today = timezone.now().date()
-    # Retrieve all IncomeExpense records created today
-    income_expenses = IncomeExpense.objects.filter(user_id=request.user).order_by('-date')
-    return render(request, 'moneymap/income-expenses.html',
-                  {'income_expenses': income_expenses})
-    # TODO: today's balance, model's default boolean is false ?
+    # Retrieve all IncomeExpense records created today for the logged-in user
+    income_expenses = IncomeExpense.objects.filter(user_id=request.user,
+                                                   date=today).order_by('date',
+                                                                        'IncomeExpense_id')
+
+    balance = 0
+    check = 0
+    income_expense_with_balance = []
+
+    for item in income_expenses:
+        # Update balance based on the type of transaction
+        if item.type == 'Income':
+            balance += item.amount
+            check += item.amount
+        else:
+            balance -= item.amount
+            check -= item.amount
+
+
+        # Add each record along with the updated balance to the list
+        income_expense_with_balance.append({
+            'description': item.description,
+            'amount': item.amount,
+            'balance': abs(balance),
+            'check': check,
+        })
+
+    return render(request, 'moneymap/income-expenses.html', {
+        'income_expense_with_balance': income_expense_with_balance
+    })
 
 
 def goals(request):
