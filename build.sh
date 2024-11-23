a60d2cc41665e5c13a1a7fe8f2b788718aff3290
Buravit Yenjit <khiaokrub@gmail.com>
2024-11-23 22:55:10 +0700

    Add automate creating a Django super user

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# Creating a custom management command
+python manage.py createsuperuser2 --username admin2 --password mysecretadminpassword
