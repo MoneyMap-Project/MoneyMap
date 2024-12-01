3e1fbca547c3a33c6e3b741906b23a57a6dd5da1
Chayakarn Hengsuwan <113860728+colarrbear@users.noreply.github.com>
2024-12-02 03:14:34 +0700

    fix days remaining

--- moneymap/service_addgoals.py
+++ moneymap/service_addgoals.py
@@ -1,11 +1,9 @@
 from django.utils import timezone
 
 
-#TODO: Since these mostly similar to the service_detailgoals.py, you can combine them into one file. (Refactor).
-
 def calculate_days_remaining(end_date):
     """Calculate the days remaining until the goal deadline."""
-    return max((end_date - timezone.now().date()).days, 0)  #TODO: We also have `days_remaining` method in the Goal model.
+    return max((end_date - timezone.now().date()).days, 0)
 
 def calculate_average_saving(current_amount, current_total_days):
     """Calculate the average saving per day to reach the goal."""
